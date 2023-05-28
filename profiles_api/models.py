ae88d6d705496ecdde5c590b21fa34012dbb49ed
Nachop51 <nachoperalta0@gmail.com>
2023-05-28 12:00:56 -0300

    Added user profile feed api

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
+from django.conf import settings
 
 # Create your models here.
 
@@ -61,3 +62,17 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         """ Return string representation of our user """
         return self.email
+
+
+class ProfileFeedItem(models.Model):
+    """ Profile status update """
+    user_profile = models.ForeignKey(
+        settings.AUTH_USER_MODEL, # Django settings
+        on_delete=models.CASCADE # if the user is deleted, the profile feed item will be deleted
+    )
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True) # automatically set the date time when we create a new object
+
+    def __str__(self):
+        """ Return the model as a string """
+        return self.status_text
--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -6,6 +6,7 @@ from profiles_api import views # Import our views from profiles_api/views.py
 router = DefaultRouter() # Create a router object
 router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset') # Register our viewset with the router
 router.register('profile', views.UserProfileViewSet) # Register our viewset with the router
+router.register('feed', views.UserProfileFeedViewSet)
 
 # Router is used to register our viewsets with the Django REST framework
 # while the path function is used to register our APIView with the Django REST framework
